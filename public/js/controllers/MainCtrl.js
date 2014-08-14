58d6c6a5842f092a62de6f974040cf14c77a99e2
Christian Le <christianle@berkeley.edu>
2014-08-14 09:31:34 -0700

    frickin touch

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,5 @@
 angular.module('MainCtrl', ['ngTouch']).controller('MainController', function($scope) {
-  $('.menu-toggle').on('click', function() {
-    console.log('here');
+  $scope.menuToggle = function() {
     $('#wrapper').toggleClass('toggled');
-  });
+  }
 });

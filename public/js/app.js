3a972462a4941122d0f6e46407d5c99497e47764
Christian Le <christianle@berkeley.edu>
2014-09-02 00:55:09 -0700

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -5,3 +5,16 @@
 // LinkedIn: http://www.linkedin.com/in/christianle94/
 
 var innovativeDesign = angular.module('innovativeDesign', ['ngRoute', 'ngTouch']);
+
+innovativeDesign.directive("scroll", function ($window) {
+  return function(scope, element, attrs) {
+    angular.element($window).bind("scroll", function() {
+      if (this.pageYOffset >= 50) {
+        scope.changeScroll = true;
+      } else {
+        scope.changeScroll = false;
+      }
+      scope.$apply();
+    });
+  };
+});

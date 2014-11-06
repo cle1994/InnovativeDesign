18ab056df38a74874efeee35f182cd2482c43172
Christian Le <christianle@berkeley.edu>
2014-11-06 14:58:37 -0800

    scroll fix

--- js/app.js
+++ js/app.js
@@ -2,11 +2,21 @@ var app = angular.module('innovativeDesign', ['ui.router', 'ngTouch', 'ngAnimate
 
 app.run(['$rootScope', '$state', '$location', '$window', function ($rootScope, $state, $location, $window) {
     $rootScope.$state = $state;
+    $rootScope.$on('$viewContentLoaded',function(){
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+    });
 }]);
 
-app.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", 'FacebookProvider', function($stateProvider, $urlRouterProvider, $locationProvider, FacebookProvider) {
+app.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", '$provide', 'FacebookProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $provide, FacebookProvider) {
     $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise("/");
+
+    $provide.decorator('$uiViewScroll', function ($delegate) {
+        return function (uiViewElement) {
+            var top = uiViewElement.getBoundingClientRect().top;
+            window.scrollTo(0, top);
+        }; 
+    });
 
     $stateProvider
         .state('home', {
